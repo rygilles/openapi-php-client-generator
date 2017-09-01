1ef95c3afc8d1b5c7db155431588fc1de48a8a7a
ryan_gilles <ryan@e-monsite.com>
2017-09-01 11:05:10 +0000

    WIP

---
+++ src/Rygilles/OpenApiPhpClientGenerator/Generator.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Rygilles\OpenApiGenerator;
+
+/**
+ * Class Generator
+ * @package Rygilles\OpenApiGenerator
+ */
+class Generator
+{
+	/**
+	 * Path of the OpenAPI file
+	 *
+	 * @var string
+	 */
+	protected $openApiFilePath;
+
+	/**
+	 * Path where the PHP client library files will be generated
+	 *
+	 * @var string
+	 */
+	protected $outputPath;
+
+	/**
+	 * Array of options
+	 *
+	 * @var mixed[]
+	 */
+	protected $options = [
+
+	];
+
+	/**
+	 * Generator constructor.
+	 *
+	 * @param string $openApiFilePath Path of the OpenAPI file
+	 * @param string $outputPath Path where the PHP client library files will be generated
+	 * @param mixed[] $options Array of options
+	 */
+	public function __construct($openApiFilePath, $outputPath, $options = [])
+	{
+		$this->openApiFilePath = $openApiFilePath;
+		$this->outputPath = $outputPath;
+		$this->options = array_merge($this->options, $options);
+	}
+
+	/**
+	 * Run generation of the PHP client library files
+	 */
+	public function generate()
+	{
+		// @todo Step : Load the OpenAPI schema file
+		// @todo Step : Parse the file using the right parser (json or yaml)
+		// @todo Step : Clear the output path
+		// @todo Step : Make the root directory
+		// @todo Step : Root directory : Make README.md
+		// @todo Step : Root directory : Make LICENSE.md
+		// @todo Step : Root directory : Make .gitignore
+		// @todo Step : Root directory : Make composer.json
+		// @todo Step : Root directory : Make phpunit.xml
+		// @todo Step : Make "src" directory
+		// @todo Step : "src" directory : Make "%libNamespace%" directory/subdirectory
+		// @todo Step : "%libNamespace%" subdirectory : Make "Resources" directory
+		// @todo Step : "%libNamespace%" subdirectory : Make "Managers" directory
+		// @todo Step : "Resources" directory : Make components/schemas as resources files
+		// @todo Step : "Managers" directory : Make managers files from operations tags pattern'Manager:*'
+		// @todo Step : Resources files : Make methods from operation tags pattern 'Resource:**'
+		// @todo Step : "%libNamespace% subdirectory : Make "%libName%Client.php"
+		// @todo Step : Make "tests" directory
+
+	}
+}
