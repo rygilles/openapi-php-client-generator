2c5a56d2de925a67fd54e6eecfbbcb1050aaae90
ryan_gilles <ryan@e-monsite.com>
2017-09-01 11:54:35 +0000

    WIP

---
+++ src/Rygilles/OpenApiPhpClientGenerator/GenerateCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rygilles\OpenApiGenerator;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class GenerateCommand extends Command
+{
+	/**
+	 * Configure the command options.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this
+			->setName('generate')
+			->setDescription('Generate PHP client files.');
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
+	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 * @return void
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$output->writeln('<info>Test...</info>');
+	}
+}
