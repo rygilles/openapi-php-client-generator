920738bb04500fbc7120803fa3495fd0dc842d76
ryan_gilles <ryan@e-monsite.com>
2017-09-01 13:06:52 +0000

    WIP

--- src/Rygilles/OpenApiPhpClientGenerator/GenerateCommand.php
+++ src/Rygilles/OpenApiPhpClientGenerator/GenerateCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rygilles\OpenApiGenerator;
+namespace Rygilles\OpenApiPhpClientGenerator;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
