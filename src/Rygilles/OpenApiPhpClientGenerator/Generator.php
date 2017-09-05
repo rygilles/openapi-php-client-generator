8b2ed87eb3a97f4fde27e62038f07976fcb9f337
ryan_gilles <ryan@e-monsite.com>
2017-09-05 07:53:38 +0000

    WIP

--- src/Rygilles/OpenApiPhpClientGenerator/Generator.php
+++ src/Rygilles/OpenApiPhpClientGenerator/Generator.php
@@ -213,12 +213,11 @@ class Generator
 						// Resolver not supported here
 						throw new Exception('Reference object in requestBody is not supported' . "\n" . 'Path: ' . $path . ', HTTP Method: ' . $httpMethod);
 					} else {
-						die(print_r($operation['requestBody']['content'], true));
-						if (count($operation['requestBody']['content']) > 1) {
+						if (count($operation['requestBody']['content']) > 0) {
 							$firstContentKey = array_keys($operation['requestBody']['content'])[0];
 							$firstContent = array_shift($operation['requestBody']['content']);
 							$schema = $firstContent['schema'];
-
+							
 							$orderedParameters = [];
 
 							// Place required parameters first
