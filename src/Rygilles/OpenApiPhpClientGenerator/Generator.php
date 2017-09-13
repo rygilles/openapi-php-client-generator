f507513c565fdd755c25866a2b728f124873ad11
ryan_gilles <ryan@e-monsite.com>
2017-09-13 12:48:56 +0000

    WIP

--- src/Rygilles/OpenApiPhpClientGenerator/Generator.php
+++ src/Rygilles/OpenApiPhpClientGenerator/Generator.php
@@ -770,6 +770,8 @@ class Generator
 						$callBody .= str_repeat("\t", $newTabs + 1) . 'return ';
 						$callBody .= $subMaker . '; ' . "\n";
 						$callBody .= str_repeat("\t", $newTabs) . '}, $requestBody' . $arrayContext . '[\'' . $property['name'] . '\']' . '), ' . "\n";
+					} else {
+						$callBody .= str_repeat("\t", $newTabs) . 'null, ' . "\n";
 					}
 				}
 				elseif (isset($property['type']) && isset($this->resourcesData[$property['type']])) {
@@ -797,6 +799,8 @@ class Generator
 
 						if ($subMaker != 'norec') {
 							$callBody .= $subMaker . ', ' . "\n";
+						} else {
+							$callBody .= 'null, ' . "\n";
 						}
 					} else {
 						if ($isArrayResponse) {
@@ -807,6 +811,8 @@ class Generator
 							);
 							if ($subMaker != 'norec') {
 								$callBody .= str_repeat("\t", $newTabs) . '(isset($data' . $arrayContext . '[\'' . $property['name'] . '\']' . ') ? (' . $subMaker . ') : null), ' . "\n";
+							} else {
+								$callBody .= str_repeat("\t", $newTabs) . 'null, ' . "\n";
 							}
 						} else {
 							$subMaker = $this->computeOperationResponsesMaker(
@@ -816,6 +822,8 @@ class Generator
 							);
 							if ($subMaker != 'norec') {
 								$callBody .= str_repeat("\t", $newTabs) . '(isset($requestBody' . $arrayContext . '[\'' . $property['name'] . '\']' . ') ? (' . $subMaker . ') : null), ' . "\n";
+							} else {
+								$callBody .= str_repeat("\t", $newTabs) . 'null, ' . "\n";
 							}
 						}
 					}
